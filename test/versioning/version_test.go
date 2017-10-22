aeb649874f904bca2dd327c0789da19529294f92
Claudio Zizza <claudio@budgegeria.de>
2017-10-22 21:49:45 +0200

    Rename package version to versioning

--- test/versioning/version_test.go
+++ test/versioning/version_test.go
@@ -1,31 +1,31 @@
-package version
+package versioning
 
 import (
 	"testing"
-	"../../src/version"
+	"../../src/versioning"
 )
 
 func TestGetWithMajor(t *testing.T) {
-	expectedVersion(version.Version{Major: 42}, "42.0.0", t)
+	expectedVersion(versioning.Version{Major: 42}, "42.0.0", t)
 }
 
 func TestGetWithMinor(t *testing.T) {
-	expectedVersion(version.Version{Major: 2, Minor: 1}, "2.1.0", t)
+	expectedVersion(versioning.Version{Major: 2, Minor: 1}, "2.1.0", t)
 }
 
 func TestGetWithPatch(t *testing.T) {
-	expectedVersion(version.Version{Major: 2, Minor: 1, Patch: 9}, "2.1.9", t)
+	expectedVersion(versioning.Version{Major: 2, Minor: 1, Patch: 9}, "2.1.9", t)
 }
 
 func TestGetWithLabel(t *testing.T) {
-	expectedVersion(version.Version{Major: 2, Minor: 1, Patch: 9, Label: "beta"}, "2.1.9-beta", t)
+	expectedVersion(versioning.Version{Major: 2, Minor: 1, Patch: 9, Label: "beta"}, "2.1.9-beta", t)
 }
 
 func TestGetWithIdentifier(t *testing.T) {
-	expectedVersion(version.Version{Major: 2, Minor: 1, Patch: 9, Label: "beta", Identifier: 4}, "2.1.9-beta.4", t)
+	expectedVersion(versioning.Version{Major: 2, Minor: 1, Patch: 9, Label: "beta", Identifier: 4}, "2.1.9-beta.4", t)
 }
 
-func expectedVersion(version version.Version, expected string, t *testing.T) {
+func expectedVersion(version versioning.Version, expected string, t *testing.T) {
 	versionStr := version.Get()
 
 	if expected != versionStr {
