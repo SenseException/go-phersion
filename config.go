02fbd142a63ccd1a8165c574b21f67d9cbd3d70e
Claudio Zizza <claudio@budgegeria.de>
2017-11-02 23:32:45 +0100

    Create config handling file

---
+++ config.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+	"strings"
+)
+
+func Init(filePath string) {
+	os.MkdirAll(filePath, 0644)
+}
+
+func Exists(dirPath string) bool {
+	dirInfo, dirNotFound := os.Stat(dirPath)
+	fileInfo, fileNotFound := os.Stat(getFilePath(dirPath))
+
+	dirExists := os.IsExist(dirNotFound) || dirInfo.IsDir()
+	fileExists := os.IsExist(fileNotFound) || ! fileInfo.IsDir()
+
+	return dirExists && fileExists
+}
+
+func getFilePath(dirPath string) string {
+	dirPath = strings.TrimRight(dirPath, os.PathSeparator)
+
+	return dirPath + os.PathSeparator + "config.json"
+}
