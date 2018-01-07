9a4a8dd25fa22809cceaffdfcbbafdf865808f49
Claudio Zizza <claudio@budgegeria.de>
2018-01-07 01:03:29 +0100

    Create cli command structure

--- main.go
+++ main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
-	"flag"
 	"github.com/SenseException/go-phersion/config"
+	"gopkg.in/urfave/cli.v1"
+	"os"
 )
 
 func main() {
-	// Define arguments
-	dirPath := flag.String("config", "./.go-pherson", "Path to version config file directory")
-	flag.Parse()
+	// path to the go-phersion config file
+	var configPath string
 
-	// Retrieve command
-	//command := flag.Arg(0)
+	// Create cli API of go-phersion
+	app := cli.NewApp()
+	app.Name = "go-phersion"
+	app.Author = "Claudio Zizza"
+	app.Description = "Managing versions in builds of projects"
 
-	var configPath string = *dirPath
 
-	if ! config.Exists(configPath) {
-		config.Init(configPath)
+	// Define arguments of go-phersion
+	app.Flags = []cli.Flag {
+		cli.StringFlag{
+			Name: "config, c",
+			Value: "./.go-pherson",
+			Usage: "Path to version config file directory",
+			Destination: &configPath,
+		},
 	}
+
+	// Create commands of go-phersion
+	app.Commands = []cli.Command {
+		{
+			Name: "init",
+			Usage: "Initialize version for a new project",
+			Action: func(c *cli.Context) error {
+				if ! config.Exists(configPath) {
+					config.Init(configPath)
+				}
+
+				return nil
+			},
+		},
+	}
+
+	app.Run(os.Args)
 }
